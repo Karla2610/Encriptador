aab2a2e4c971a77a0987f5ea85ba10fc408efbfa
Karla Chaves <karlachaves97@gmail.com>
2023-05-26 19:05:24 -0600

    correcciones y ajustes

--- script.js
+++ script.js
@@ -85,9 +85,43 @@ function desencriptarTexto(mensaje){
     return textoFinal;
 }
 
+function removerAcentos(area_texto){
+    const acentos = [  
+    /[\300-\306]/g, /[\340-\346]/g, 
+    /[\310-\313]/g, /[\350-\353]/g, 
+    /[\314-\317]/g, /[\354-\357]/g,
+    /[\322-\330]/g, /[\362-\370]/g, 
+    /[\331-\334]/g, /[\371-\374]/g, 
+    /[\321]/g, /[\361]/g, 
+    /[\307]/g, /[\347]/g, 
+  ];
+  
+  const letras = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
+  
+  for (let i = 0; i < acentos.length; i++) {
+    area_texto = area_texto.replace(acentos[i], letras[i]);
+  }
+  
+  return area_texto;
+}
+
+function validandoTexto(area_texto) {
+  const normalizar-area_texto = removerAcentos(area_texto);
+  
+  if (area_texto !== normalizar-area_texto) {
+    alert('No se aceptan acentos en el texto. Por favor, remueve los acentos antes de continuar.');
+  } else {
+    console.log('Texto válido:', area_texto);
+  }
+}
+
+const inputText = '¡Hólà!';
+validateText(inputText);
+
+
 let btnCopiar = document.querySelector(".btn-Copiar");
     btnCopiar.addEventListener("click", copiar = () => {
     let contenido = document.querySelector(".texto-resultado").textContent;
     navigator.clipboard.writeText(contenido);
-    alert("texto copiado con exito")
+    alert("¡texto copiado con exito!")
 })
